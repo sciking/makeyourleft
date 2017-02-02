92392e8fb0b43a5be777045b5292e02bf783c7c1
Mike Sciking <sciking@users.noreply.github.com>
2017-02-02 15:53:19 +0100

    Update myl.py

--- myl.py
+++ myl.py
@@ -5,6 +5,7 @@ import random
 turno = 0
 planzi = 31
 ptua = 4
+spar = 1
 ptot = planzi + ptua
 def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')
@@ -17,8 +18,16 @@ chiaramente governato da un'oligarchia di
 destra che non pensa ai lavoratori.
 
 Ogni 4 turni si terrà il congresso, nel quale potrai candidarti segretario,
-proporre iniziative o dichiarare la scissione"""
+proporre iniziative o dichiarare la scissione.
+Ma attenzione, se alle elezioni che si tengono ogni 9 turni
+non avrai almeno il 4% dovrai ritirarti dalla vita politica!"""
+
 
 def gioco():
+	global ptua,planzi.ptot,turno
+	ptot = ptua = planzi
 	turno = turno +1
-	
+	if spar == 1:
+		print "Unione Democratica - Sistema Informatico"
+	else:
+		print "Federazione Nuova Sinistra - Sistema Informatico"
