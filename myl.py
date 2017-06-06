ac646ee7d5eb246e22e10e8ca816426e4886dc64
Mike Sciking <sciking@users.noreply.github.com>
2017-06-06 21:03:42 +0200

    Update myl.py

--- myl.py
+++ myl.py
@@ -7,7 +7,6 @@ planzi = 31
 ptua = 4
 alpar = 65
 spar = 1
-ptot = planzi + ptua
 def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')
 def invio():
@@ -23,12 +22,13 @@ Ogni 4 turni si terrà il congresso, nel quale potrai candidarti segretario,
 proporre iniziative o dichiarare la scissione.
 Ma attenzione, se alle elezioni che si tengono ogni 9 turni
 non avrai almeno il 4% dovrai ritirarti dalla vita politica!"""
-
+print ptua
 invio()
 clear()
 def gioco():
-	global ptua,planzi,ptot,turno,alpar
-	alpar = alpar + random.randint(-3,3)
+	global ptua,planzi,turno,alpar,spar
+
+	alpar = alpar + random.randint(3,3)
 	if ptua < 0:
 		ptua = 0
 	if planzi < 0:
@@ -39,22 +39,24 @@ def gioco():
 		print "Unione Democratica - Sistema Informatico"
 	else:
 		print "Federazione Nuova Sinistra - Sistema Informatico"
-	planzi = planzi + random.randint(-2,2)
-	ptua = ptua + random.randint(-1,1)
+	planzi = planzi + random.randint(-5,5)
+	#ptua = ptua + random.randint(-1,1)
 	#qui inizia il giornale
 	gior = random.randint(1,10)
+
 	print("La Sinistra Unita - Giornale dei Lavoratori")
+
 	if gior == 1:
 		print "Matteo Lanzi: Bisogna cambiare la Sinista"
-		planzi = planzi + random.randint(-2,2)
-		ptua = ptua + random.randint(-1,1)
+		planzi = planzi + random.randint(-2,4)
+		ptua = ptua + random.randint(-1,2)
 	elif gior == 2:
 		print "La svolta a sinistra va bene, molti la vorrebbero"
 		ptua = ptua + 2
-		planzi = planzi - 2
+		planzi = planzi - 3
 	elif gior == 3:
 		print "Cristiani e Democratici entra nell'Unione Democratica \n Pieno supporto a Lanzi"
-		planzi = planzi + 3
+		planzi = planzi + 4
 	elif gior == 4:
 		print "Flop linea Lanzi, persi 3 capoluoghi tra cui Agrusco, roccaforte rossa"
 		planzi = planzi + 3
@@ -92,7 +94,43 @@ def gioco():
 		print "Elezione probabile"
 	else:
 		print "Elezione a rischio"
+
+
+#congresso
+	if turno%4 == 0 and spar == 1:
+		print "CONGRESSO DI UNIONE DEMOCRATICA"
+		print("""Opzioni:
+1) Candidati segretario
+2) Dichiara la scissione
+3) Intervento pro-segreteria
+4) Intervento istituzionale
+5) Non recarti al Congresso""")
+		l = input(">")
+		if l == 1:
+			if ptua > planzi and random.randint(1,10) > 4:
+				print("Sei stato eletto segretario!")
+				vittoria()
+			else:
+				print("Perdi la segreteria")
+				ptua = ptua + random.randint(-4,6)
+				planzi = plandi + random.randint(-6,7)
+		elif l == 2:
+			print("L'Osservatore del Parlamento\nRivoluzione a Sinistra! Dichiarata la scissione\nNasce Nuova Sinistra")
+			spar = 0
+		elif l == 3:
+			print("L'Osservatore del Parlamento\nIl Collettivo Nuova Sinistra supporta Lanzi")
+			ptua = ptua + random.randint(-2,5)
+			planzi = plandi + random.randint(-3,6)
+		elif l == 4:
+			print("L'Osservatore del Parlamento\nIl famoso deputato di Nuova Sinistra: 'Viva la Democrazia'")
+			ptua = ptua + random.randint(-1,2)
+		elif l == 5:
+			print("L'Osservatore del Parlamento\nStrappo di Nuova Sinistra? Mancanti al congresso")
 	invio()
 	clear()
 	gioco()
+
+def vittoria():
+		print("Pota")
+		exit()
 gioco()
